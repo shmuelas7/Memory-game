403347b140db794cdacbeee5299baf606728b931
shmuel asherov <shmuelas7@gmail.com>
2022-04-17 13:45:10 +0300

    add card by size

--- main.js
+++ main.js
@@ -1,27 +1,15 @@
-// const cards = ["a", "b", "c", "a", "b", "c"];
-const pokemon = [
-	{ id: 4, name: "Charmander", type: "fire", base_experience: 62 },
-	{ id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-	{ id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-	{ id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
-	{ id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
-	{ id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-	{ id: 94, name: "Gengar", type: "poison", base_experience: 225 },
-	{ id: 133, name: "Eevee", type: "normal", base_experience: 65 },
-	{ id: 4, name: "Charmander", type: "fire", base_experience: 62 },
-	{ id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-	{ id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-	{ id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
-	{ id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
-	{ id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-	{ id: 94, name: "Gengar", type: "poison", base_experience: 225 },
-	{ id: 133, name: "Eevee", type: "normal", base_experience: 65 },
-];
+const size = Number(prompt("enter size"));
+const arr = [];
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+function getCard(size) {
+	for (let i = 1; i <= size; i++) {
+		let imgSrc = `${POKE_API}${padToThree(i)}.png`;
+		arr.push({ id: i, img: imgSrc });
+	}
+}
 
 const board = document.getElementById("board");
-
-const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
-let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
 function shuffle(array) {
 	let currentIndex = array.length,
@@ -41,36 +29,26 @@ function shuffle(array) {
 }
 function creatCards(arr) {
 	for (let card of arr) {
-		let imgSrc = `${POKE_API}${padToThree(card.id)}.png`;
-
 		const div = document.createElement("div");
-		const name = document.createElement("h1");
-		const img = document.createElement("img");
-		const type = document.createElement("div");
-		const exp = document.createElement("div");
-
+		const fimg = document.createElement("img");
+		const bimg = document.createElement("img");
 		div.className = "Pokecard";
 		div.style.cursor = "pointer";
+		console.log(card.id);
+		div.id = card.id;
+		div.onclick = "flip";
+		// bimg.src = imgSrc;
+		bimg.className = "back";
+		console.log(card.img);
+		fimg.src = card.img;
+		fimg.className = "back";
 
-		name.innerHTML = card.name;
-		img.src = imgSrc;
-		img.onmouseover = function () {
-			this.style.height = "40%";
-		};
-		img.onmouseleave = function () {
-			this.style.height = "60%";
-		};
-		type.className = "Pokecard-data";
-		type.innerHTML = "Type: " + card.type;
+		div.appendChild(fimg);
 
-		exp.className = "Pokecard-data";
-		exp.innerHTML = "Power: " + card.base_experience;
-		div.appendChild(name);
-		div.appendChild(img);
-		div.appendChild(type);
-		div.appendChild(exp);
 		board.appendChild(div);
 	}
 }
+getCard(size);
+const pokemon = [...arr, ...arr];
 shuffle(pokemon);
 creatCards(pokemon);
