be06e50309c02287275bbca71f434b81141f9c2f
shmuel asherov <shmuelas7@gmail.com>
2022-04-26 19:42:06 +0300

    changes cards

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
-const size = Number(prompt("enter size"));
-const arr = [];
-let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
-const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
-function getCard(size) {
-	for (let i = 1; i <= size; i++) {
-		let imgSrc = `${POKE_API}${padToThree(i)}.png`;
-		arr.push({ id: i, img: imgSrc });
-	}
-}
+// const size = Number(prompt("enter size"));
+const arr = [
+	{ id: 1, img: "./pokemon/p1.png" },
+	{ id: 2, img: "./pokemon/p2.png" },
+	{ id: 3, img: "./pokemon/p3.png" },
+	{ id: 4, img: "./pokemon/p4.png" },
+	{ id: 5, img: "./pokemon/p5.png" },
+	{ id: 6, img: "./pokemon/p6.png" },
+	{ id: 7, img: "./pokemon/p7.png" },
+	{ id: 8, img: "./pokemon/p8.png" },
+	{ id: 9, img: "./pokemon/p9.png" },
+	{ id: 10, img: "./pokemon/p10.png" },
+	{ id: 11, img: "./pokemon/p11.png" },
+	{ id: 12, img: "./pokemon/p12.png" },
+	{ id: 13, img: "./pokemon/p13.png" },
+	{ id: 14, img: "./pokemon/p14.png" },
+	{ id: 15, img: "./pokemon/p15.png" },
+	{ id: 16, img: "./pokemon/p16.png" },
+	{ id: 17, img: "./pokemon/p17.png" },
+	{ id: 18, img: "./pokemon/p18.png" },
+	{ id: 19, img: "./pokemon/p19.png" },
+	{ id: 20, img: "./pokemon/p20.png" },
+	{ id: 21, img: "./pokemon/p21.png" },
+	{ id: 22, img: "./pokemon/p22.png" },
+	{ id: 23, img: "./pokemon/p23.png" },
+	{ id: 24, img: "./pokemon/p24.png" },
+	{ id: 25, img: "./pokemon/p25.png" },
+	{ id: 26, img: "./pokemon/p26.png" },
+	{ id: 27, img: "./pokemon/p27.png" },
+	{ id: 28, img: "./pokemon/p28.png" },
+	{ id: 29, img: "./pokemon/p29.png" },
+	{ id: 30, img: "./pokemon/p30.png" },
+];
 
 const board = document.getElementById("board");
 
@@ -29,6 +52,7 @@ function shuffle(array) {
 }
 function chack(id1, id2) {
 	if (id1 !== id2) {
+		console.log(first);
 		console.log(secend);
 		let a = document.getElementById(id1);
 		a.className = "face";
@@ -40,14 +64,23 @@ function chack(id1, id2) {
 
 		console.log(a);
 		console.log(b);
-		first = "";
-		secend = "";
+	}
+	first = "";
+	secend = "";
+	return;
+}
+function returnImg(id) {
+	for (x of arr) {
+		if (id == x.id) {
+			return x.img;
+		}
 	}
 }
 function creatCards(arr) {
 	for (let pok of arr) {
 		const card = document.createElement("div");
-
+		const a = document.createElement("a");
+		a.download = pok.img;
 		const fimg = document.createElement("img");
 
 		card.className = "Pokecard";
@@ -59,22 +92,24 @@ function creatCards(arr) {
 		card.appendChild(fimg);
 		board.appendChild(card);
 		card.addEventListener("click", (e) => {
-			if (fimg.className === "face") {
+			console.log(e);
+			if (fimg.className === "face" && (first === "" || secend === "")) {
 				fimg.classList = "back";
-				fimg.src = pok.img;
+
+				fimg.src = returnImg(e.target.id);
 				if (first === "") {
 					first = e.target.id;
 				} else {
 					secend = e.target.id;
 					setTimeout(function () {
 						chack(first, secend);
-					}, 2000);
+					}, 500);
 				}
 			}
 		});
 	}
 }
-getCard(size);
+// getCard(size);
 const pokemon = [...arr, ...arr];
 let first = "";
 let secend = "";
